55bb669361c6767f708fe89a1b0046ae9c3aa2e2
emarque <emarquez@h2scan.com>
2018-06-23 12:50:57 -0700

    better table-cell

--- js/breaking-news-ticker.js
+++ js/breaking-news-ticker.js
@@ -412,14 +412,11 @@
                 'margin-top': 30,
                 'opacity' : 0
             }).show();
-            
-            _ul.find('li').eq(_activeNews).css({
-                'display': table-cell
-            });
 
             _ul.find('li').eq(_activeNews).animate({
                 'margin-top': 0,
-                'opacity' : 1
+                'opacity' : 1,
+                'display' : 'table-cell'
             },ticker.settings.animateSpeed);
         }
 
