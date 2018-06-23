3910167949a1f6483f589197dda72efe7383e675
emarque <emarquez@h2scan.com>
2018-06-23 01:11:13 -0700

    tidying

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
 $(document).ready(function() {
-	//feed to parse
-	var feed = "https://feed.rssunify.com/5b2b311136f50/rss.xml";
-	$.ajax(feed, {
-		accepts:{
-			xml:"application/rss+xml"
-		},
-		dataType:"xml",
-		success:function(data) {
-			//Credit: http://stackoverflow.com/questions/10943544/how-to-parse-an-rss-feed-using-javascript
+    //feed to parse
+    var feed = "https://feed.rssunify.com/5b2b311136f50/rss.xml";
+    $.ajax(feed, {
+        accepts: {
+            xml: "application/rss+xml"
+        },
+        dataType: "xml",
+        success: function(data) {
+            //Credit: http://stackoverflow.com/questions/10943544/how-to-parse-an-rss-feed-using-javascript
 
-			$(data).find("item").each(function () { // or "item" or whatever suits your feed
-        		var el=$(this);
-				var $item = $("<li/>",{
-          class: "item"
-        });
-        var $title = $("<div class='title'>" + el.find("title").text() + "</div>");
-					var $description = $("<div class='description'>" + el.find("description").text() + "</div>");
-          
-          $description = $(".feedflare, img, p:gt(0)",$description).remove().end();
-          $item.append($title);
-          $item.append($description);
-          
-          if($title.text().length > 0 && $description.text().length > 0){
-						$item.appendTo($("#items"));
-          }
-			}); 
-		}
-	});
-  $('#newsfeed').breakingNews({
-		effect: 'slide-up'
-	});
+            $(data).find("item").each(function() { // or "item" or whatever suits your feed
+                var el = $(this);
+                var $item = $("<li/>", {
+                    class: "item"
+                });
+                var $title = $("<div class='title'>" + el.find("title").text() + "</div>");
+                var $description = $("<div class='description'>" + el.find("description").text() + "</div>");
+
+                $description = $(".feedflare, img, p:gt(0)", $description).remove().end();
+                $item.append($title);
+                $item.append($description);
+
+                if ($title.text().length > 0 && $description.text().length > 0) {
+                    $item.appendTo($("#items"));
+                }
+            });
+        }
+    });
+    $('#newsfeed').breakingNews({
+        effect: 'slide-up'
+    });
 });
